e40872eb309c6bd0e866563bad29631f20e72957
Kyle Salmon <kyleraysalmon@gmail.com>
2022-11-16 13:48:29 -0800

    styles

--- src/components/LocationSearch/index.tsx
+++ src/components/LocationSearch/index.tsx
@@ -1,13 +1,14 @@
 
  // @ts-nocheck
 import * as React from 'react';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import { styled } from '@mui/system';
+import InputBase from '@mui/material/InputBase';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import "./LocationSearch.css"
 
 interface LocationSearch {
   onHandleSelect?: (data: any) => void
@@ -19,58 +20,70 @@ const LocationSearch = ({ address, onHandleSelect }: LocationSearch) => {
     setAddress(address);
   };
 
-  const handleSelect = address => {
-    geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => {
-        onHandleSelect({ address, ...latLng })
-      })
-      .catch(error => console.error('Error', error));
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    debounce: 300,
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    console.log(e.target.value)
+    setValue(e.target.value);
   };
 
-  const StyledContainer = styled('div')`
-  width: 300px;
-  `
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        console.log("📍 Coordinates: ", { lat, lng });
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  console.log(data)
 
   return (
-    
-    <PlacesAutocomplete
-      value={address}
-      onChange={handleChange}
-      onSelect={handleSelect}
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <div>
-        
-          <Autocomplete
-            freeSolo
-            disablePortal
-            disableClearable
-            options={suggestions.map((option) => option.description)}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label={"Find Location"}
-                onChange={(data) => handleChange(data.target.value)}
-                onSelect={(data) => handleSelect(data.target.value)}
-                sx={{
-                  width: 300,
-                }}
-                {...getInputProps({
-                  
-                  placeholder: 'Enter location',
-                  className: 'location-search-input',
-                })}
-              />
-            )}
+      <Autocomplete
+        freeSolo
+        disableClearable
+        options={data.map((option) => option.description)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Search for location..."
+            value={value}
+            sx={{ height: 50, width: 300 }}
+            onChange={handleInput}
+            shrink={false}
           />
-          
-        </div>
-      )}
-    </PlacesAutocomplete>
-    
+        )}
+      />  
   );
-  
 }
 
 export default LocationSearch;
