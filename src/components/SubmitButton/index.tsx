342047b85c1bdc49f6b208db18982b5458b016d5
Kyle Salmon <kyleraysalmon@gmail.com>
2022-11-17 12:40:31 -0800

    Zindex and Bold Submit

--- src/components/SubmitButton/index.tsx
+++ src/components/SubmitButton/index.tsx
@@ -6,22 +6,23 @@ interface SubmitButton {
   disabled?: boolean;
 }
 const SubmitButton = ({ disabled = false }: SubmitButton) => {
-
   const StyledButton = styled(Button)`
     font-family: 'Lato – Solace', 'Lato', sans-serif;
+    font-weight: 700;
     height: 50px;
     background: #1D4339;
     border-radius: 10px;
     padding: 14px 40px !important;
     text-transform: none;
+    z-index: 100;
     
     span {
-    font-style: normal;
-    font-weight: 700;
-    padding: 14px 30px;
-    font-size: 18px;
-    line-height: 22px;
-    color: #FFFFFF;
+      font-style: normal;
+      font-weight: 700;
+      padding: 14px 30px;
+      font-size: 18px;
+      line-height: 22px;
+      color: #FFFFFF;
     }
 
     &:hover {
