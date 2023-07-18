41e00e354f77f996d9776fa75aceafba985eeffd
Alyssa Antonaccio <alyssa@solace.health>
2023-07-18 11:02:18 -0400

    [SOL-1520] Update Entry Widget for new Funnel Improvements

--- src/WhoAreYouHereFor.tsx
+++ src/WhoAreYouHereFor.tsx
@@ -3,30 +3,25 @@ import styled from '@emotion/styled';
 import { Icons, NextButton } from './components';
 import { ButtonBase } from '@mui/material';
 import { useFormContext } from 'react-hook-form';
-import { animated, useSpring, config } from "@react-spring/web";
+import { animated, useSpring, config } from '@react-spring/web';
 
 const StyledHeader = styled.div`
   font-size: 30px;
   line-height: 34px;
-  font-family: "Mollie glaston";
-  font-feature-settings: "liga" 1, "kern";
+  font-family: 'Mollie glaston';
+  font-feature-settings: 'liga' 1, 'kern';
   font-weight: 400;
   text-align: center;
   letter-spacing: 0.005em;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
 `;
 
 const StyledButton = styled(ButtonBase)<{ margin?: string }>`
   display: flex;
   flex-direction: column;
   width: 100%;
-  font-family: "Lato-Solace", "Lato – Solace", "Lato", sans-serif;
+  font-family: 'Lato-Solace', 'Lato – Solace', 'Lato', sans-serif;
   font-weight: 500;
-  border: 1.5px solid #3F937C;
+  border: 1.5px solid #3f937c;
   background-color: #fff;
   color: #101010;
   text-transform: none;
@@ -36,7 +31,7 @@ const StyledButton = styled(ButtonBase)<{ margin?: string }>`
   font-size: 20px;
   line-height: 20px;
   padding: 15px;
-  margin: ${(props) => (props.margin ? props.margin : "20px 0 0 0")};
+  margin: ${props => (props.margin ? props.margin : '20px 0 0 0')};
 
   span {
     margin-top: 12px;
@@ -79,79 +74,50 @@ const StyledButton = styled(ButtonBase)<{ margin?: string }>`
   }
 `;
 
-const StyledError = styled(animated.div)`
-  font-size: 14px;
-  color: red;
-  padding: 5px 0 0;
+const LinkContainer = styled.div`
+  color: #101010;
+  font-family: Lato;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 12.558px;
+  letter-spacing: 0.157px;
 `;
 
-interface Props {
-  next: () => void;
-  style: unknown;
-}
+const StyledLink = styled.a`
+  color: #285e50;
+  text-decoration: none;
+`;
 
-const WhoAreYouHereFor = ({ next, style }: Props) => {
-  const { setValue, getValues } = useFormContext();
-  const [showError, setShowError] = React.useState(false);
-
-  const onSubmit = () => {
-    const hereFor = getValues("hereFor");
-
-    if (!hereFor) {
-      setShowError(true);
-      return;
-    }
-
-    setShowError(false);
-    next();
-  };
-
-  const errorStyle = useSpring(
-    showError
-      ? {
-          opacity: 1,
-          height: 15,
-          transform: "translate(0px, 0px)",
-          display: "block",
-          config: config.gentle,
-        }
-      : {
-          opacity: 0,
-          height: 0,
-          transform: "translate(0px, -2px)",
-          display: "none",
-          config: config.gentle,
-        }
-  );
-
+const WhoAreYouHereFor = ({ onSubmit }: { onSubmit: (v: { [hereFor: string]: string }) => void }) => {
   return (
-    <animated.div style={style}>
+    <div>
       <StyledHeader>Who are you here for?</StyledHeader>
-      <StyledError style={errorStyle}>Please choose an option below to continue.</StyledError>
       <StyledButton
-        margin="36px 0 20px"
+        margin='36px 0 24px'
         onClick={() => {
-          setValue('hereFor', 'self');
-          next();
+          onSubmit({ hereFor: 'self' });
         }}
       >
         <Icons.SunIcon /> <span>Myself</span>
       </StyledButton>
       <StyledButton
-        margin="0 0 36px"
+        margin='0 0 24px'
         onClick={() => {
-          setValue('hereFor', 'loved_one');
-          next();
+          onSubmit({ hereFor: 'loved_one' });
         }}
       >
         <Icons.HeartIcon />
         <span> My Loved One</span>
       </StyledButton>
 
-      <ButtonContainer>
-        <NextButton next={onSubmit} />
-      </ButtonContainer>
-    </animated.div>
+      <LinkContainer>
+        Already have an account?{' '}
+        <StyledLink href='https://app.solace.health/login' target='_blank' rel='noreferrer'>
+          Log in
+        </StyledLink>
+      </LinkContainer>
+    </div>
   );
 };
 
